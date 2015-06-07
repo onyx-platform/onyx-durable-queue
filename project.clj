cd18fce28643fd3b7703f52a6014566f9857a004
Michael Drogalis <mjd3089@rit.edu>
2015-06-07 09:21:11 -0700

    Revert back to RC1

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [org.onyxplatform/onyx "0.6.0"]
+                 [org.onyxplatform/onyx "0.6.0-RC1"]
                  [factual/durable-queue "0.1.5"]]
   :profiles {:dev {:dependencies [[midje "1.6.3"]]
                    :plugins [[lein-midje "3.1.1"]]}})
