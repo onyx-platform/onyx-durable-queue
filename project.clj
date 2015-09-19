7bbbfaf12f7496a0e3aa818c2852819de631d70f
Michael Drogalis <mjd3089@rit.edu>
2015-09-19 15:07:36 -0700

    Move up to next Onyx version.

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject org.onyxplatform/onyx-durable-queue "0.7.0"
+(defproject org.onyxplatform/onyx-durable-queue "0.7.4-SNAPSHOT"
   :description "Onyx plugin for Factual's durable-queue"
   :url "https://github.com/onyx-platform/onyx-durable-queue"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.onyxplatform/onyx "0.7.0"]
+                 ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
+                 [org.onyxplatform/onyx "0.7.4-20150916_205458-gf33df5b"]
                  [factual/durable-queue "0.1.5"]]
   :profiles {:dev {:dependencies [[midje "1.7.0"]]
                    :plugins [[lein-midje "3.1.1"]]}})
