23e2fd5cbc65a391d5d42d984a8285980e377bc5
Michael Drogalis <mjd3089@rit.edu>
2015-09-20 11:53:42 -0700

    Release version 0.7.4.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-durable-queue "0.7.4-SNAPSHOT"
+(defproject org.onyxplatform/onyx-durable-queue "0.7.4"
   :description "Onyx plugin for Factual's durable-queue"
   :url "https://github.com/onyx-platform/onyx-durable-queue"
   :license {:name "Eclipse Public License"
