b8b34497ee3db64890da43a70791cafd631bccbd
OnyxBot <michael.drogalis@onyxplatform.org>
2015-09-30 06:42:41 +0000

    Update dependency org.onyxplatform/onyx to version 0.7.10.
    Automatic commit by lein-unison.

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
                              :sign-releases false}}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                 [org.onyxplatform/onyx "0.7.8-20150930_061332-g9bf0f1b"]
+                 [org.onyxplatform/onyx "0.7.10"]
                  [factual/durable-queue "0.1.5"]]
   :profiles {:dev {:dependencies [[midje "1.7.0"]]
                    :plugins [[lein-midje "3.1.1"]
