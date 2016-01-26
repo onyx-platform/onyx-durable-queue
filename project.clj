5ef145abac5d4acb30fa91aa689226671d4bd907
OnyxBot <michael.drogalis@onyxplatform.org>
2016-01-26 16:54:13 +0000

    Prepare for next release cycle.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-durable-queue "0.8.5.0"
+(defproject org.onyxplatform/onyx-durable-queue "0.8.5.1-SNAPSHOT"
   :description "Onyx plugin for Factual's durable-queue"
   :url "https://github.com/onyx-platform/onyx-durable-queue"
   :license {:name "Eclipse Public License"
